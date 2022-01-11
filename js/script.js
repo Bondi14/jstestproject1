5ab5534391848a40c7d781160d4dbd652e0fdba0
Bondi14 <haysak.andrij@gmail.com>
2022-01-11 20:19:23 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
 
-const person = "Andrew";
+// const person = "Andrew";
 
-const obj = {
-    name: "Ivan",
-    age: 25,
-    isMarried: false
-};
+// const obj = {
+//     name: "Ivan",
+//     age: 25,
+//     isMarried: false
+// };
 
-console.log(obj.name);
+// console.log(obj.name);
 
-let arr = ['apple', 'orange', 6];
-console.log(arr[1]);
+// let arr = ['apple', 'orange', 6];
+// console.log(arr[1]);
+
+//const result = confirm("what?");
+
+// const answer = +prompt("what?", "100");
+// console.log(typeof(answer));
+
+const answers =[];
+
+answers[0] = prompt('what?', '');
+answers[1] = prompt('the?', '');
+answers[2] = prompt('hack?', '');
+
