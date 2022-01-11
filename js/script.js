558003e18ef7d264c660115a5ca2aeb6f4290072
Bondi14 <haysak.andrij@gmail.com>
2022-01-11 18:58:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+
+const person = "Andrew";
+
+const obj = {
+    name: "Ivan",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['apple', 'orange', 6];
+console.log(arr[1]);
