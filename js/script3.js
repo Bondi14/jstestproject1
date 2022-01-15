1eff16fc9db1c05db6736ae60b79dd6049bbb576
Bondi14 <haysak.andrij@gmail.com>
2022-01-15 17:47:51 +0200

    loop added

---
+++ js/script3.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// if (4 == 4) {
+//     console.log('ok');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log('Error');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+// const num = 55;
+
+// switch (num) {
+//     case 49:
+//         console.log('Error');
+//         break;
+//     case 51:
+//         console.log('Error');
+//         break;
+//     case 50:
+//         console.log('Ok');
+//         break;
+//     default:
+//         console.log('later');
+//         break;
+// }
+
+
