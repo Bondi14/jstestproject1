db2135c188653d072c615fbd23522bdc30c88503
Bondi14 <haysak.andrij@gmail.com>
2022-01-16 13:13:16 +0200

    app2

--- js/script2.js
+++ js/script2.js
@@ -8,14 +8,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const   a = prompt('Last film?', ''),
-        b = prompt('rate?', ''),
-        c = prompt('Last film?', ''),
-        d = prompt('rate?', '');
-
         //personalMovieDB.movies.a = b;  - неправильно запишуться дані
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Last film?', ''),
+          b = prompt('rate?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("bad");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("norm");
+} else if (personalMovieDB.count >= 30) {
+    console.log("cool");
+} else {
+    console.log("error");
+}
 
 console.log(personalMovieDB);
 
